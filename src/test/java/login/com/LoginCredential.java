ec2fd0293234e1286c19ef80f4a18306d4d61704
GVR <GVR@DESKTOP-I1F5JMB>
2023-04-26 12:30:03 +0530

    second one

--- src/test/java/login/com/LoginCredential.java
+++ src/test/java/login/com/LoginCredential.java
@@ -1,8 +1,16 @@
 package login.com;
 
 public class LoginCredential {
+	
+	private void anna() {
+	System.out.println("just method");
+
+	}
 
 	public static void main(String[] args) {
+		LoginCredential a= new LoginCredential();
+		a.anna();
+				
 		System.out.println("hello world");
 	}
 }
